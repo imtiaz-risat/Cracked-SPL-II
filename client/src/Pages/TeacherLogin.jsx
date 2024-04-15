e24d08a9198f2cfda73431dc34e8f9c1f1df43f9
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-04-15 15:27:39 +0600

    TeacherLogin and TeacherRegister modified (cyan)

--- client/src/Pages/TeacherLogin.jsx
+++ client/src/Pages/TeacherLogin.jsx
@@ -1,7 +1,6 @@
 import { useForm } from "react-hook-form";
-import crackEdLogo from "../Assets/CrackEd-logo.png";
-import { useNavigate } from 'react-router-dom';
-
+import crackEdLogo from "../Assets/CrackEd-cyan-dark-logo.png";
+import { useNavigate } from "react-router-dom";
 
 export default function TeacherLogin() {
   const {
@@ -9,30 +8,28 @@ export default function TeacherLogin() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  
+
   const navigate = useNavigate();
   const onSubmit = async (data) => {
-   
-    await fetch('http://localhost:8000/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+    await fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Submission was successful');
-      console.log(data);
-      
-       navigate('/teacher/dashboard');
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Submission was successful");
+        console.log(data);
+
+        navigate("/teacher/dashboard");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
-  
   return (
     <section className="bg-gray-50 ">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -44,7 +41,7 @@ export default function TeacherLogin() {
         </a>
         <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-cyan-800 md:text-2xl ">
               Sign in to your account
             </h1>
             <form
@@ -54,13 +51,13 @@ export default function TeacherLogin() {
               <div>
                 <label
                   htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 "
+                  className="block mb-2 text-sm font-medium text-cyan-700 "
                 >
                   Your email
                 </label>
                 <input
                   type="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  className="bg-gray-50 border border-cyan-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                   placeholder="name@company.com"
                   {...register("email", { required: true })}
                 />
@@ -72,14 +69,14 @@ export default function TeacherLogin() {
               <div>
                 <label
                   htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900"
+                  className="block mb-2 text-sm font-medium text-cyan-700"
                 >
                   Password
                 </label>
                 <input
                   type="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  className="bg-gray-50 border border-cyan-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   {...register("password", {
                     required: true,
                     minLength: {
@@ -97,33 +94,33 @@ export default function TeacherLogin() {
                   <div className="flex items-center h-5">
                     <input
                       type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                      className="w-4 h-4 border border-cyan-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
                       {...register("rememberMe")}
                     />
                   </div>
                   <div className="ml-3 text-sm">
-                    <label htmlFor="remember" className="text-gray-500 ">
+                    <label htmlFor="remember" className="text-cyan-500 ">
                       Remember me
                     </label>
                   </div>
                 </div>
                 <a
                   href="#"
-                  className="text-sm font-medium text-primary-600 hover:underline "
+                  className="text-sm font-medium text-cyan-600 hover:underline "
                 >
                   Forgot password?
                 </a>
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-[#6b7280] hover:bg-[#374151] focus:ring-4 focus:outline-none focus:ring-[#d1d5db] font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                className="w-full text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-[#d1d5db] font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 Sign in
               </button>
-              <p className="text-sm font-light text-gray-500 ">
+              <p className="text-sm font-light text-cyan-500 ">
                 Don’t have an account yet?{" "}
                 <a
-                  href="/register"
+                  href="/tutor-register"
                   className="font-medium text-primary-600 hover:underline "
                 >
                   Sign up
--- client/src/Pages/TeacherRegister.jsx
+++ client/src/Pages/TeacherRegister.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from 'react-router-dom';
-import crackEdLogo from "../Assets/CrackEd-logo.png";
+import { useNavigate } from "react-router-dom";
+import crackEdLogo from "../Assets/CrackEd-cyan-dark-logo.png";
 
 export default function TeacherRegister() {
   const {
@@ -10,65 +10,64 @@ export default function TeacherRegister() {
     formState: { errors },
     setError,
   } = useForm();
-  
 
   const navigate = useNavigate();
 
-  const onSubmit = async(data) => {
+  const onSubmit = async (data) => {
     if (!(data.password === data.confirmPassword)) {
-        setError("root.confirmPassword", {
-            type: "invalid",
-            message: "Confirm Password doesn't match",
-        });
-        return;
+      setError("root.confirmPassword", {
+        type: "invalid",
+        message: "Confirm Password doesn't match",
+      });
+      return;
     }
     if (data.termAndConditions === false) {
-        setError("root.termAndConditions", {
-            type: "required",
-            message: "You must accept the term and conditions to signup",
-        });
-        return;
+      setError("root.termAndConditions", {
+        type: "required",
+        message: "You must accept the term and conditions to signup",
+      });
+      return;
     }
 
-    await fetch('http://localhost:8000/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+    await fetch("http://localhost:8000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Email or username already exists');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Submission was successful');
-      console.log(data);
-      
-       navigate('/teacher/dashboard');
-    })
-    .catch((error) => {
-      setError("root.email", {
-        type: "invalid",
-        message: error.message,
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Email or username already exists");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Submission was successful");
+        console.log(data);
+
+        navigate("/teacher/dashboard");
+      })
+      .catch((error) => {
+        setError("root.email", {
+          type: "invalid",
+          message: error.message,
+        });
+        setError("root.username", {
+          type: "invalid",
+          message: error.message,
+        });
+        if (error.message === "Email or username already exists") {
+          window.alert(
+            "You already have an account with this email or username."
+          );
+        }
       });
-      setError("root.username", {
-        type: "invalid",
-        message: error.message,
-      });
-      if (error.message === 'Email or username already exists') {
-        window.alert('You already have an account with this email or username.');
-      }
-    });
 
     console.log(data);
-};
+  };
   return (
     <section className="bg-gray-50 ">
-     
-
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="/"
@@ -78,8 +77,8 @@ export default function TeacherRegister() {
         </a>
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Create an account
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-cyan-800 md:text-2xl">
+              Create a Tutor Account
             </h1>
             <form
               className="space-y-2 md:space-y-2"
@@ -88,13 +87,13 @@ export default function TeacherRegister() {
               <div>
                 <label
                   htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900"
+                  className="block mb-2 text-sm font-medium text-cyan-700"
                 >
                   Your email
                 </label>
                 <input
                   type="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  className="bg-gray-50 border border-cyan-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="name@company.com"
                   {...register("email", { required: true })}
                 />
@@ -105,13 +104,13 @@ export default function TeacherRegister() {
               <div>
                 <label
                   htmlFor="username"
-                  className="block mb-2 text-sm font-medium text-gray-900"
+                  className="block mb-2 text-sm font-medium text-cyan-700"
                 >
                   Username
                 </label>
                 <input
                   type="text"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  className="bg-gray-50 border border-cyan-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="choose an username"
                   {...register("username", { required: true })}
                 />
@@ -122,14 +121,14 @@ export default function TeacherRegister() {
               <div>
                 <label
                   htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900"
+                  className="block mb-2 text-sm font-medium text-cyan-700"
                 >
                   Password
                 </label>
                 <input
                   type="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  className="bg-gray-50 border border-cyan-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   {...register("password", {
                     required: true,
                     minLength: {
@@ -145,14 +144,14 @@ export default function TeacherRegister() {
               <div>
                 <label
                   htmlFor="confirm-password"
-                  className="block mb-2 text-sm font-medium text-gray-900"
+                  className="block mb-2 text-sm font-medium text-cyan-700"
                 >
                   Confirm password
                 </label>
                 <input
                   type="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  className="bg-gray-50 border border-cyan-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   {...register("confirmPassword", { required: true })}
                 />
                 {errors?.root?.confirmPassword?.message && (
@@ -166,14 +165,15 @@ export default function TeacherRegister() {
                 <div className="flex items-center h-5">
                   <input
                     type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                    className="w-4 h-4 border border-cyan-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
                     {...register("termAndConditions")}
                   />
                 </div>
                 <div className="ml-3 text-sm">
-                  <label htmlFor="terms" className="font-light text-gray-500">
+                  <label htmlFor="terms" className="font-light text-cyan-500">
                     I accept the{" "}
-                    <a className="font-medium text-primary-600 hover:underline"
+                    <a
+                      className="font-medium text-cyan-600 hover:underline"
                       href="#"
                     >
                       Terms and Conditions
@@ -188,14 +188,14 @@ export default function TeacherRegister() {
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-[#6b7280] hover:bg-[#374151] focus:ring-4 focus:outline-none focus:ring-[#d1d5db] font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                className="w-full text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-[#d1d5db] font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
-                Create an account
+                Become a Tutor
               </button>
-              <p className="text-sm font-light text-gray-500 ">
+              <p className="text-sm font-light text-cyan-500 ">
                 Already have an account?{" "}
                 <a
-                  href="/login"
+                  href="/tutor-login"
                   className="font-medium text-primary-600 hover:underline "
                 >
                   Login here
