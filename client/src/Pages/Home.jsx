65cf75b877b0865bad4330022c3fc6e5e9e12a1b
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-05-02 15:14:14 +0600

    Toggle Menu added

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,13 +1,20 @@
+import { redirect } from "react-router-dom";
 import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
 import NavBar from "../components/NavBar";
 import OurTutors from "../components/OurTutors";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 export default function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
-      <NavBar />
+      <NavBar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
       <button onClick={() => toast.success("Hello")}>Click me</button>
       <HeroSection />
       <OurTutors />
